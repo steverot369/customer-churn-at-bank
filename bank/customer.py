7cbbe55ca2f82cef35ecc5be90c60f25d6472f58
steverot369 <steverodrigues369@gmail.com>
2023-06-26 20:57:16 +0530

    new chnages

--- bank/customer.py
+++ bank/customer.py
@@ -15,14 +15,61 @@ def customerhome():
     cursor = db.cursor()
     cursor.execute("SELECT t_no,t_type,amount,date_time,customer_id from transaction where customer_id=(select cid from customers where cid='%s') LIMIT 8"%(session['cust_id']))
     transaction = cursor.fetchall()
-    return render_template('customerhome.html',transaction=transaction)
+    cursor.execute("select fname,lname,email from customers where loginid='%s'"%(session['logid']))
+    name = cursor.fetchall()
+    return render_template('customerhome.html',transaction=transaction,name=name)
 
 
 
 @customer.route('/customerviewtransaction')
 def customerviewtransaction():
     # data={}
-    return render_template('customerviewtransaction.html')
+    cursor = db.cursor()
+
+    cursor.execute("SELECT t_no,t_type,amount,date_time,customer_id from transaction where customer_id=(select cid from customers where cid='%s')"%(session['cust_id']))
+    transaction = cursor.fetchall()
+    return render_template('customerviewtransaction.html',transaction=transaction)
+
+
+@customer.route('/customerviewaccount')
+def customerviewaccount():
+    # data={}
+    cursor = db.cursor()
+    cursor.execute("SELECT acc_no,balance,acc_started_date from savingsacc where customer_id=(select cid from customers where cid='%s')"%(session['cust_id']))
+    accounts = cursor.fetchall()
+    return render_template('customerviewaccount.html',accounts=accounts)
+
+
+@customer.route('/customertransferfund')
+def customertransferfund():
+    # data={}
+    return render_template('customertransferfund.html')
+
+
+@customer.route('/customersetpin')
+def customersetpin():
+    # data={}
+    return render_template('customersetpin.html')
+
+
+@customer.route('/customerpayloan')
+def customerpayloan():
+    # data={}
+    return render_template('customerpayloan.html')
+
+
+@customer.route('/customerviewloanpayments')
+def customerviewloanpayments():
+    # data={}
+    return render_template('customerviewloanpayments.html')
+
+
+
+
+@customer.route('/customerrequestcreditcard')
+def customerrequestcreditcard():
+    # data={}
+    return render_template('customerrequestcreditcard.html')
 
 
 
